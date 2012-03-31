b2574ae7233b7491896603d7e00815e140a91df0
Mariusz Cieśla <mariusz@dotmariusz.net>
2012-03-31 12:14:49 +0200

    New .vimrc, also Irssi config and an OSX hack in shell

---
+++ osx.sh
@@ -0,0 +1,9 @@
+defaults write com.apple.dock no-glass -bool true
+defaults write NSGlobalDomain AppleFontSmoothing -int 1
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write com.apple.screencapture disable-shadow -bool true
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
