0e3960af7d35270f96318a1bbb6039229780e33b
Mariusz Cieśla <mariusz@dotmariusz.net>
2012-03-31 12:19:40 +0200

    Documenting OSX.sh

--- osx.sh
+++ osx.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
+echo "✓ Enable 2D dock"
 defaults write com.apple.dock no-glass -bool true
-defaults write NSGlobalDomain AppleFontSmoothing -int 1
+
+echo "✓ Enable Apple font-smoothing on non-Apple displays"
+defaults write NSGlobalDomain AppleFontSmoothing -int 2
+
+echo "✓ Enable tab switching in modal windows and other keyboard navigation perks"
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+echo "✓ Enable current directory as the default scope for Finder search"
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+echo "✓ Expand 'Save' panel by default"
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+echo "✓ Expand 'Print' panel by default"
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+
+echo "✓ Disable shadow in screenshots"
 defaults write com.apple.screencapture disable-shadow -bool true
+
+echo "✓ Disable spelling correction"
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
