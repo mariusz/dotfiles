d4f34b7da566dc8cebab7946381c62e78c44ebda
Mariusz Ciesla <mariusz@dotmariusz.net>
2023-06-08 13:54:09 +0200

    Add aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+alias g="git"
+alias ga="git add"
+alias gc="git commit"
+alias gp="git push"
+alias gr="git rebase"
+
+source ~/.work.fish
